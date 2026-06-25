d4a95cee2fddf7c43d6aba4dc0d4a1a04c61184c
waled84413-sudo <waled84413@gmail.com>
2026-06-25 12:29:56 +0200

    Create Main.java

---
+++ run/Main.java
@@ -0,0 +1,16 @@
+package run;//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+import gui.LibraryGUI;
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new LibraryGUI());
+    }
+}
